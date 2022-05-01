55708430a4b92054189de5833a673223aeb73cef
robonexx <robertwagar@gmail.com>
2022-05-01 11:50:35 +0200

    mixins for fonts and spacing,

--- src/App.js
+++ src/App.js
@@ -2,8 +2,40 @@ import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Responsiveness and design library</h1>
+    <div className='App'>
+      <header>
+        <h2>HEADER LOGO</h2>
+      </header>
+      <nav>
+        <ul>
+          <li>
+            <a>Home</a>
+          </li>
+          <li>
+            <a>about</a>
+          </li>
+          <li>
+            <a>page</a>
+          </li>
+        </ul>
+      </nav>
+      <div className='app-top'>
+        <h1>Responsiveness and design library</h1>
+
+        <h2>Learning as I go reading thru blogs and also Udemy courses</h2>
+
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsam dolor
+          doloribus, saepe vel repellendus optio omnis culpa. Doloremque itaque
+          commodi facere, libero atque quos! Amet deleniti accusamus placeat
+          ipsam dolorum quis eum quisquam harum, error, obcaecati earum voluptas
+          magnam dolores!
+        </p>
+      </div>
+      <div className='app-bottom'>
+        <div className='bottom-left'>Left</div>
+        <div className='bottom-right'>Right</div>
+      </div>
     </div>
   );
 }
